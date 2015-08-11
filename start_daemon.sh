0edf1bbf823269b33bc68daf29f72c07e8fcad19
roy.lieu <roy.lieu@gmail.com>
2015-08-11 20:07:58 +0800

    bug-fix: change the logic of Flag: LogToStdout

--- start_daemon.sh
+++ start_daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup ./message_service -log_to_stdout=true -server_debug=false -force_gc=true -force_gc_period=60 -force_free_os_memory=false &
+nohup ./message_service -log_to_stdout=false -server_debug=false -force_gc=true -force_gc_period=60 -force_free_os_memory=false &
