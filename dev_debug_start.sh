2ed9779a0b9664ac87292f418445e6f740a287c1
roy.lieu <roy.lieu@gmail.com>
2015-08-14 17:13:17 +0800

    modify start_deamon.sh and dev_debug_start.sh

--- dev_debug_start.sh
+++ dev_debug_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./message_service -log_to_stdout=true -server_debug=true -force_gc=true -force_gc_period=60 -force_free_os_memory=true
+./message_service -log_to_stdout=true -server_debug=true -force_gc=true -force_gc_period=60 -force_free_os_memory=true -keepalive=true
