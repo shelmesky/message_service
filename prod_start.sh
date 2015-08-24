a8c1919add66c2151173c0ee3df2a743e3c1be48
roy.lieu <roy.lieu@gmail.com>
2015-08-24 20:16:21 +0800

    modify start script

--- prod_start.sh
+++ prod_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./message_service -log_to_stdout=false -server_debug=false -force_gc=true -force_gc_period=60 -force_free_os_memory=false
+./message_service -log_to_stdout=false -server_debug=false -force_gc=true -force_gc_period=60 -force_free_os_memory=false -keepalive=true
